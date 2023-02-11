52bc18d2a94621c663382dc4a0f948a495a5e26e
Melih Kara <79717410+reynisterro77@users.noreply.github.com>
2023-02-11 12:00:53 +0300

    Add files via upload

---
+++ Fotoğraf Galerisi/main.js
@@ -0,0 +1,36 @@
+var index=0;
+
+show_image(index);
+
+function show_image(i){
+    index +=i;
+    // console.log(index);
+
+    var images=document.getElementsByClassName("image");
+    var dots=document.getElementsByClassName("dot");
+
+    for (i=0;i<images.length;i++){
+        images[i].style.display="none";
+        // console.log(images[i]);
+    }
+
+    for (i=0;i<dots.length;i++){
+        dots[i].className=dots[i].className.replace(" active","");
+        // console.log(dots[i]);
+    }
+
+    if (index>images.length-1){
+        index=0;
+    }
+
+    if (index<0){
+        index=images.length-1;
+    }
+
+    images[index].style.display="block";
+    dots[index].className+=" active";
+
+
+}
+
+
